46d3c0dcc7e536fb436fca99182f3be847ee6941
ext4cats <80286360+ext4cats@users.noreply.github.com>
2024-09-28 04:47:35 -0300

    set up basic browse button handlers

--- PortableInstaller/MainWindow.xaml.cs
+++ PortableInstaller/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace PortableInstaller;
 
@@ -19,5 +20,50 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void TargetExecutableBrowseButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "Select Target Executable...",
+            Filter = "Executable files (*.exe)|*.exe"
+        };
+
+        if (dialog.ShowDialog() == true)
+        {
+            TargetExecutableInput.Text = dialog.FileName;            
+        }
+    }
+
+    private void UseParentDirectoryCheckBox_OnChecked(object sender, RoutedEventArgs e)
+    {
+        ParentDirectoryInput.IsEnabled = true;
+        ParentDirectoryBrowseButton.IsEnabled = true;
+    }
+
+    private void UseParentDirectoryCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        ParentDirectoryInput.IsEnabled = false;
+        ParentDirectoryBrowseButton.IsEnabled = false;
+    }
+
+    private void ParentDirectoryBrowseButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Title = "Select Parent Directory...",
+            InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        };
+
+        if (dialog.ShowDialog() == true)
+        {
+            ParentDirectoryInput.Text = dialog.FolderName;
+        }
+    }
+
+    private void InstallButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
